126242b5fe3a5f3b71a9fc7ba14724aafe81c779
kaiser.ouyang <1031918012@qq.com>
2022-04-08 15:21:00 +0800

    1

--- aspnetapp/Controllers/CounterController.cs
+++ aspnetapp/Controllers/CounterController.cs
@@ -8,22 +8,75 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using aspnetapp;
 
+public class CounterRequest
+{
+    public string action { get; set; }
+}
+public class CounterResponse
+{
+    public int data { get; set; }
+}
+
 namespace aspnetapp.Controllers
 {
-    [Route("api/[Controller]")]
+    [Route("api/count")]
     [ApiController]
     public class CounterController : ControllerBase
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="reqGetAbnormal"></param>
-        /// <returns></returns>
-        [HttpPost("GetAbnormalPage")]
-        public async Task<string> GetAbnormalPage()
+        private readonly CounterContext _context;
+
+        public CounterController(CounterContext context)
         {
-            Console.WriteLine("这个接口接受消息成功了");
-            return "";
+            _context = context;
+        }
+        private async Task<Counter> getCounterWithInit()
+        {
+            var counters = await _context.Counters.ToListAsync();
+            if (counters.Count() > 0)
+            {
+                return counters[0];
+            }
+            else
+            {
+                var counter = new Counter { count = 0, createdAt = DateTime.Now, updatedAt = DateTime.Now };
+                _context.Counters.Add(counter);
+                await _context.SaveChangesAsync();
+                return counter;
+            }
+        }
+        // GET: api/count
+        [HttpGet]
+        public async Task<ActionResult<CounterResponse>> GetCounter()
+        {
+            var counter = await getCounterWithInit();
+            return new CounterResponse { data = counter.count };
+        }
+
+        // POST: api/Counter
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<CounterResponse>> PostCounter(CounterRequest data)
+        {
+            if (data.action == "inc")
+            {
+                var counter = await getCounterWithInit();
+                counter.count += 1;
+                counter.updatedAt = DateTime.Now;
+                await _context.SaveChangesAsync();
+                return new CounterResponse { data = counter.count };
+            }
+            else if (data.action == "clear")
+            {
+                var counter = await getCounterWithInit();
+                counter.count = 0;
+                counter.updatedAt = DateTime.Now;
+                await _context.SaveChangesAsync();
+                return new CounterResponse { data = counter.count };
+            }
+            else
+            {
+                return BadRequest();
+            }
         }
     }
 }
