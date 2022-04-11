1498f16cab1dfb654c20abf567ad4e321e8cb51a
kaiser.ouyang <1031918012@qq.com>
2022-04-11 13:42:10 +0800

    1

--- aspnetapp/Controllers/MessageController.cs
+++ aspnetapp/Controllers/MessageController.cs
@@ -15,34 +15,12 @@ namespace aspnetapp.Controllers
     [ApiController]
     public class MessageController : ControllerBase
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="reqGetAbnormal"></param>
-        /// <returns></returns>
-        [HttpPost("GetAbnormalPage")]
-        public string GetAbnormalPage()
-        {
-            Console.WriteLine("这个接口接受消息成功了");
-            return "1";
-        }
-
         /// <returns></returns>
         [HttpPost("message")]
-        public string Message([FromBody]MessageModel messageModel)
+        public string Message()
         {
-            var a = JsonConvert.SerializeObject(messageModel);
-            Console.WriteLine(a);
-            return a;
-        }
-        public class MessageModel
-        {
-            public string ToUserName { get; set; }
-            public string FromUserName { get; set; }
-            public string CreateTime { get; set; }
-            public string MsgType { get; set; }
-            public string Content { get; set; }
-            public string MsgId { get; set; }
+            Console.WriteLine("转发消息成功了");
+            return "";
         }
     }
 }
